87aef2b20318501d6923399b2a8a4081a400d9b2
Kimiko Kano <kskano@gmail.com>
2015-12-10 23:58:02 -0700

    Bubble Sort

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -8,28 +8,21 @@ class BubbleSort
   end
 
   def sort
-    previous, current = 0, 1
-    while current <= array.length
-      if array[previous] > array[current] # ~> ArgumentError: comparison of String with nil failed
-        @array[previous], @array[current] = @array[current], @array[previous] # => ["b", "d"], ["a", "d"]
-
-        array # => ["b", "d", "a"], ["b", "a", "d"]
+    array.length.times do
+      previous, current = 0, 1
+      while current < array.length
+        if array[previous] > array[current]
+          @array[previous], @array[current] = @array[current], @array[previous]
+        end
+        previous += 1
+        current += 1
       end
-      previous += 1 # => 1, 2
-      current += 1 # => 2, 3
-      
     end
+    array
   end
 end
 
 if __FILE__ == $0
-sorter = BubbleSort.new(["d", "b", "a"])
+sorter = BubbleSort.new(["d", "b", "a", "c"])
 sorter.sort
 end
-
-# ~> ArgumentError
-# ~> comparison of String with nil failed
-# ~>
-# ~> /Users/kimiko/Documents/Turing/1module/projects/sorting_suite/lib/bubble_sort.rb:13:in `>'
-# ~> /Users/kimiko/Documents/Turing/1module/projects/sorting_suite/lib/bubble_sort.rb:13:in `sort'
-# ~> /Users/kimiko/Documents/Turing/1module/projects/sorting_suite/lib/bubble_sort.rb:26:in `<main>'
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -13,9 +13,13 @@ class BubbleSortTest < Minitest::Test
   end
 
   def test_can_sort_an_empty_array
-    skip
     sorted = BubbleSort.new([])
     assert_equal [], sorted.sort
+  end
+
+  def test_can_sort_an_array_of_one
+    sorted = BubbleSort.new(["a"])
+    assert_equal ["a"], sorted.sort
   end
 
   def test_can_sort_an_array_of_two
@@ -26,5 +30,15 @@ class BubbleSortTest < Minitest::Test
   def test_can_sort_an_array_of_three
     sorted = BubbleSort.new(["d", "b", "a"])
     assert_equal ["a", "b", "d"], sorted.sort
+  end
+
+  def test_can_sort_an_array_of_four
+    sorted = BubbleSort.new(["d", "b", "a", "c"])
+    assert_equal ["a", "b", "c", "d"], sorted.sort
+  end
+
+  def test_can_sort_an_array_of_six_numbers
+    sorted = BubbleSort.new([48, 23, 63, 54, 3, 20])
+    assert_equal [3, 20, 23, 48, 54, 63], sorted.sort
   end
 end
