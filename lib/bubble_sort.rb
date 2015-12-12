6672151406cc29f7ebdda5580a3358919ddc9ece
Kimiko Kano <kskano@gmail.com>
2015-12-11 23:09:49 -0700

    Bubble sort refactor

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,18 +1,12 @@
 require 'pry'
 
 class BubbleSort
-  attr_reader :array
-
-  def initialize(array)
-    @array = array
-  end
-
-  def sort
+  def sort(array)
     array.length.times do
       previous, current = 0, 1
       while current < array.length
         if array[previous] > array[current]
-          @array[previous], @array[current] = @array[current], @array[previous]
+          array[previous], array[current] = array[current], array[previous]
         end
         previous += 1
         current += 1
@@ -23,6 +17,6 @@ class BubbleSort
 end
 
 if __FILE__ == $0
-sorter = BubbleSort.new(["d", "b", "a", "c"])
-sorter.sort
+sorter = BubbleSort.new
+sorter.sort(["d", "b", "a", "c"])
 end
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -3,42 +3,59 @@ require 'minitest/pride'
 require 'bubble_sort'
 
 class BubbleSortTest < Minitest::Test
+  def setup
+    @sorted = BubbleSort.new
+  end
+
   def test_class_exists
     assert BubbleSort
   end
 
   def test_can_create_instances
-    sorted = BubbleSort.new([])
-    assert_instance_of BubbleSort, sorted
+    assert_instance_of BubbleSort, @sorted
   end
 
   def test_can_sort_an_empty_array
-    sorted = BubbleSort.new([])
-    assert_equal [], sorted.sort
+    assert_equal [], @sorted.sort([])
   end
 
   def test_can_sort_an_array_of_one
-    sorted = BubbleSort.new(["a"])
-    assert_equal ["a"], sorted.sort
+    assert_equal ["a"], @sorted.sort(["a"])
   end
 
-  def test_can_sort_an_array_of_two
-    sorted = BubbleSort.new(["d", "b"])
-    assert_equal ["b", "d"], sorted.sort
+  def test_can_sort_an_array_of_two_1
+    assert_equal ["b", "d"], @sorted.sort(["d", "b"])
   end
 
-  def test_can_sort_an_array_of_three
-    sorted = BubbleSort.new(["d", "b", "a"])
-    assert_equal ["a", "b", "d"], sorted.sort
+  def test_can_sort_an_array_of_two_2
+    assert_equal ["b", "d"], @sorted.sort(["b", "d"])
   end
 
-  def test_can_sort_an_array_of_four
-    sorted = BubbleSort.new(["d", "b", "a", "c"])
-    assert_equal ["a", "b", "c", "d"], sorted.sort
+  def test_can_sort_an_array_of_three_1
+    assert_equal ["a", "b", "d"], @sorted.sort(["d", "b", "a"])
+  end
+
+  def test_can_sort_an_array_of_three_2
+    assert_equal ["a", "b", "d"], @sorted.sort(["d", "a", "b"])
+  end
+
+  def test_can_sort_an_array_of_three_3
+    assert_equal ["a", "b", "d"], @sorted.sort(["b", "d", "a"])
+  end
+
+  def test_can_sort_an_array_of_four_1
+    assert_equal ["a", "b", "c", "d"], @sorted.sort(["d", "b", "a", "c"])
+  end
+
+  def test_can_sort_an_array_of_four_2
+    assert_equal ["a", "b", "c", "d"], @sorted.sort(["c", "b", "d", "a"])
+  end
+
+  def test_can_sort_an_array_of_four_3
+    assert_equal ["a", "a", "b", "d"], @sorted.sort(["a", "b", "d", "a"])
   end
 
   def test_can_sort_an_array_of_six_numbers
-    sorted = BubbleSort.new([48, 23, 63, 54, 3, 20])
-    assert_equal [3, 20, 23, 48, 54, 63], sorted.sort
+    assert_equal [3, 20, 23, 48, 54, 63], @sorted.sort([48, 23, 63, 54, 3, 20])
   end
 end
